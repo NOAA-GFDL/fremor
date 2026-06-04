9a34ca4778313169b8da85c0c81c27313eaf6cb6
Frank Malatino <frank.malatino@noaa.gov>
2026-06-04 15:26:47 -0400

    Bumping version to 0.9.6.post

--- fremor/_version.py
+++ fremor/_version.py
@@ -3,5 +3,5 @@ version info for fremor, isolated to avoid importing heavy dependencies at build
 """
 
 import os
-version = os.getenv("GIT_DESCRIBE_TAG", "0.9.6")
+version = os.getenv("GIT_DESCRIBE_TAG", "0.9.6.post")
 __version__ = version
