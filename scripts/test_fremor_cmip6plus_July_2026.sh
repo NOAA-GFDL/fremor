7de52c4495c227b28f20704f0fc8db49e1e5c0ef
Ian Laflotte <ilaflott@gmail.com>
2026-08-24 11:10:19 -0400

    update testing script

--- scripts/test_fremor_cmip6plus_July_2026.sh
+++ scripts/test_fremor_cmip6plus_July_2026.sh
@@ -7,16 +7,17 @@
 # for historical, we are going to test targeting archive files for e.g. variable list, configuration writing, etc.
 
 
+
 #### INPUT CONFIG
 #### ------------
 ## check flags, 0 --> yes, 1 --> no
-CHECK_INIT=0 # GOOD / DONE / GREAT (no surprise)
-CHECK_VARLIST=1 # GOOD / DONE / GREAT (no surprise)
-CHECK_FIND=1 # GOOD / DONE / GREAT (no surprise)
-CHECK_CONFIG=1 # looks like the strict mode thing isn't working?
-CHECK_YAML=1 #
-CHECK_RUN=1 # TODO
-CHECK_RESOLVE=1 # TODO
+CHECK_INIT=1 # GOOD
+CHECK_VARLIST=1 # GOOD
+CHECK_FIND=1 # GOOD
+CHECK_CONFIG=1 # GOOD
+CHECK_YAML=0 #
+CHECK_RUN=1 #
+CHECK_RESOLVE=1 #
 
 
 ## FYI which fremor i'm using, not strictly used nor necessary.
@@ -39,6 +40,8 @@ COMPONENT_DIR_STUB_VARLIST_ONLY=atmos_cmip/ts/monthly/43yr/
 
 PP_START=1979
 PP_STOP=2021
+
+
 
 #### DERIVED CONFIG
 #### --------------
@@ -64,133 +67,134 @@ cd "${WORKING_CWD}" || return
 # NOTE: the required input data is not required for this step, and neither is the CWD/editable info above, it's just convenience scaffolding
 FREMOR_INIT_OUTDIR=${WORKING_CWD}/fremor_init_outdir
 USER_CONFIG=${FREMOR_INIT_OUTDIR}/CMIP6plus_user_input.json
-CMIP6PLUS_TABLE_DIR=${FREMOR_INIT_OUTDIR}/table_checkout
+CMIP6PLUS_TABLES=${FREMOR_INIT_OUTDIR}/mip-cmor-tables-main/Tables
 if [[ "${CHECK_INIT}" -eq 1 ]]; then
     echo "not checking fremor init"
 else
     # fremor init - works, check!
     echo "setting up fremor init check, clobbering any prev made output"
-    rm -rf "${FREMOR_INIT_OUTDIR}" || echo "no init output to remove, OK!" && mkdir "${FREMOR_INIT_OUTDIR}" && mkdir "${CMIP6PLUS_TABLE_DIR}"
+    rm -rf "${FREMOR_INIT_OUTDIR}" || echo "no init output to remove, OK!" && mkdir "${FREMOR_INIT_OUTDIR}" && mkdir "${CMIP6PLUS_TABLES}"
 
     echo "running fremor init"
     echo "fremor init --mip_era cmip6plus --exp_config ${USER_CONFIG} -t ${FREMOR_INIT_OUTDIR} --fast"
     fremor -vv init \
            --mip_era cmip6plus \
            --exp_config "${USER_CONFIG}" \
-           -t "${CMIP6PLUS_TABLE_DIR}"
+           -t "${FREMOR_INIT_OUTDIR}" \
+		   --fast
 
     echo "checking that fremor init's output exists, return if not"
     ls -l "${USER_CONFIG}" && echo "found user config!" || return
-    ls -l "${CMIP6PLUS_TABLE_DIR}" && echo "found a tabledir with contents!" || return
+    ls -l "${CMIP6PLUS_TABLES}" && echo "found a tabledir with contents!" || return
 fi
 
 
-##### VARLIST
-##### -------
-#FREMOR_VARLIST_OUTDIR=${WORKING_CWD}/fremor_varlist_outdir #DIRECTORY
-#FREMOR_VARLIST_OUTPUT=${FREMOR_VARLIST_OUTDIR}/foo.list #FULL FILEPATH
-#if [[ "${CHECK_VARLIST}" -eq 1 ]]; then
-#    echo "not checking fremor varlist"
-#else
-#    # fremor varlist
-#    echo "setting up fremor varlist check, clobbering any prev made output"
-#    rm -rf "${FREMOR_VARLIST_OUTDIR}" || echo "no varlist output to remove, OK!" && mkdir "${FREMOR_VARLIST_OUTDIR}"
-#
-#    echo "running fremor varlist"
-#    echo "fremor -vv varlist --dir_targ ${TARG_FREBRONX_PPDIR} -o ${FREMOR_VARLIST_OUTPUT}"
-#    fremor -vv varlist \
-#           --dir_targ "${TEST_COMPONENT_DIR}" \
-#           -o "${FREMOR_VARLIST_OUTPUT}"
-#
-#    echo "checking that fremor varlist's output exists, return if not"
-#    ls -l "${FREMOR_VARLIST_OUTPUT}" || return
-#    #ls -ld
-#fi
-#
-#
-#
-##### FIND
-##### ----
-#if [[ "${CHECK_FIND}" -eq 1 ]]; then
-#    echo "not checking fremor find"
-#else
-#    echo "setting up fremor find check, which does not produce any output (no dir setup necessary)"
-#
-#    echo "running fremor find"
-#    echo "fremor -v find --table_config_dir ${CMIP7_TABLES} --varlist ${FREMOR_VARLIST_OUTPUT}"
-#    fremor -v find \
-#           --table_config_dir "${CMIP7_TABLES}" \
-#           --varlist "${FREMOR_VARLIST_OUTPUT}"
-#fi
-#
-#
-#
-##### CONFIG
-##### ------
-#FREMOR_CONFIG_OUTDIR=${WORKING_CWD}/fremor_config_outdir
-#FREMOR_CONFIG_OUTYAML=${FREMOR_CONFIG_OUTDIR}/cmor.yaml
-#if [[ "${CHECK_CONFIG}" -eq 1 ]]; then
-#    echo "not checking fremor config"
-#else
-#    echo "setting up fremor config check, clobbering any prev made output"
-#    rm -rf "${FREMOR_CONFIG_OUTDIR}" || echo "no config output to remove, OK!" && mkdir "${FREMOR_CONFIG_OUTDIR}"
-#    rm -rf "${FREMOR_VARLIST_OUTDIR}" || echo "no varlist output to remove, OK!" && mkdir "${FREMOR_VARLIST_OUTDIR}"
-#
-#    echo "running fremor config"
-#    echo "fremor -v config --pp_dir ${TARG_FREBRONX_PPDIR} --mip_tables_dir ${CMIP7_TABLES} --exp_config ${USER_CONFIG} --mip_era cmip7 --freq monthly --chunk 5yr --grid g999 --calendar noleap --output_yaml ${FREMOR_CONFIG_OUTYAML} --output_dir ${OUTPUT_CMORIZED_DATA_DIR} --varlist_dir ${FREMOR_VARLIST_OUTDIR} --strict_varlist --overwrite"
-#
-#    fremor -v config \
-#           --pp_dir "${TARG_FREBRONX_PPDIR}" \
-#           --mip_tables_dir "${CMIP7_TABLES}" \
-#           --exp_config "${USER_CONFIG}" \
-#           --mip_era "cmip7" \
-#           --freq "monthly" \
-#           --chunk "5yr" \
-#           --grid "g999" \
-#           --calendar "noleap" \
-#           --output_yaml "${FREMOR_CONFIG_OUTYAML}" \
-#           --output_dir "${OUTPUT_CMORIZED_DATA_DIR}" \
-#           --varlist_dir "${FREMOR_VARLIST_OUTDIR}" \
-#           --strict_varlist \
-#           --pp_comp_glob "*land*" \
-#           --overwrite
-#
-#
-#    echo "checking that fremor config's output exists, return if not"
-#    ls -l "${FREMOR_CONFIG_OUTYAML}" || return
-#fi
-#
-#
-##### YAML
-##### ----
-## fremor yaml itself doesn't really create output, fremor run does
-## that output directory is passed through the yaml to fremor run
-##FREMOR_YAML_OUTDIR=${OUTPUT_CMORIZED_DATA_DIR}/fremor_yaml_outdir
-#if [[ "${CHECK_YAML}" -eq 1 ]]; then
-#    echo "not checking fremor yaml"
-#else
-#    ## lets not do this for this part yet
-#    #echo "setting up fremor yaml check, clobbering any prev made output"
-#    #rm -rf "${FREMOR_YAML_OUTDIR}" || echo "no yaml output to remove, OK!" && mkdir "${FREMOR_YAML_OUTDIR}"
-#
-#
-#    echo "running fremor yaml"
-#    echo "fremor -vv yaml --yamlfile ${FREMOR_CONFIG_OUTYAML} --start ${PP_START} --stop ${PP_STOP} --print_cli_call --run_strict --run_one --dry_run"
-#    fremor -vv yaml \
-#           --yamlfile "${FREMOR_CONFIG_OUTYAML}" \
-#           --start "${PP_START}" \
-#           --stop "${PP_STOP}"
-##           --run_strict \
-##           --run_one \
-##           --dry_run
-#
-#    echo "checking the output cmorized data directory for successfully created output"
-#    tree ${OUTPUT_CMORIZED_DATA_DIR}/*/*/CMIP/
-#
-#    echo "checking the output cmorized data directory for created output"
-#    echo "number of left-behind tmp outputs (without interpolated pressure style coordinate vars is:"
-#    ls ${OUTPUT_CMORIZED_DATA_DIR}/*/*/CMOR_tmp/*nc  | grep -v '\.ps\.' | grep -v '\.phalf\.' | grep -v -c '\.pfull\.'
-#fi
+#### VARLIST
+#### -------
+FREMOR_VARLIST_OUTDIR=${WORKING_CWD}/fremor_varlist_outdir #DIRECTORY
+FREMOR_VARLIST_OUTPUT=${FREMOR_VARLIST_OUTDIR}/foo.list #FULL FILEPATH
+if [[ "${CHECK_VARLIST}" -eq 1 ]]; then
+    echo "not checking fremor varlist"
+else
+    # fremor varlist
+    echo "setting up fremor varlist check, clobbering any prev made output"
+    rm -rf "${FREMOR_VARLIST_OUTDIR}" || echo "no varlist output to remove, OK!" && mkdir "${FREMOR_VARLIST_OUTDIR}"
+
+    echo "running fremor varlist"
+    echo "fremor -vv varlist --dir_targ ${TARG_FREBRONX_PPDIR} -o ${FREMOR_VARLIST_OUTPUT}"
+    fremor -vv varlist \
+           --dir_targ "${TEST_COMPONENT_DIR}" \
+           -o "${FREMOR_VARLIST_OUTPUT}"
+
+    echo "checking that fremor varlist's output exists, return if not"
+    ls -l "${FREMOR_VARLIST_OUTPUT}" || return
+    #ls -ld
+fi
+
+
+
+#### FIND
+#### ----
+if [[ "${CHECK_FIND}" -eq 1 ]]; then
+    echo "not checking fremor find"
+else
+    echo "setting up fremor find check, which does not produce any output (no dir setup necessary)"
+
+    echo "running fremor find"
+    echo "fremor -v find --table_config_dir ${CMIP6PLUS_TABLES} --varlist ${FREMOR_VARLIST_OUTPUT}"
+    fremor -v find \
+           --table_config_dir "${CMIP6PLUS_TABLES}" \
+           --varlist "${FREMOR_VARLIST_OUTPUT}"
+fi
+
+
+
+#### CONFIG
+#### ------
+FREMOR_CONFIG_OUTDIR=${WORKING_CWD}/fremor_config_outdir
+FREMOR_CONFIG_OUTYAML=${FREMOR_CONFIG_OUTDIR}/cmor.yaml
+if [[ "${CHECK_CONFIG}" -eq 1 ]]; then
+    echo "not checking fremor config"
+else
+    echo "setting up fremor config check, clobbering any prev made output"
+    rm -rf "${FREMOR_CONFIG_OUTDIR}" || echo "no config output to remove, OK!" && mkdir "${FREMOR_CONFIG_OUTDIR}"
+    rm -rf "${FREMOR_VARLIST_OUTDIR}" || echo "no varlist output to remove, OK!" && mkdir "${FREMOR_VARLIST_OUTDIR}"
+
+    echo "running fremor config"
+    echo "fremor -v config --pp_dir ${TARG_FREBRONX_PPDIR} --mip_tables_dir ${CMIP6PLUS_TABLES} --exp_config ${USER_CONFIG} --mip_era cmip6plus --freq monthly --chunk 5yr --grid g999 --calendar noleap --output_yaml ${FREMOR_CONFIG_OUTYAML} --output_dir ${OUTPUT_CMORIZED_DATA_DIR} --varlist_dir ${FREMOR_VARLIST_OUTDIR} --strict_varlist --overwrite"
+
+    fremor -vv config \
+           --pp_dir "${TARG_FREBRONX_PPDIR}" \
+           --mip_tables_dir "${CMIP6PLUS_TABLES}" \
+           --exp_config "${USER_CONFIG}" \
+           --mip_era "cmip6plus" \
+           --freq "monthly" \
+           --chunk "43yr" \
+           --grid "g999" \
+           --calendar "julian" \
+           --output_yaml "${FREMOR_CONFIG_OUTYAML}" \
+           --output_dir "${OUTPUT_CMORIZED_DATA_DIR}" \
+           --varlist_dir "${FREMOR_VARLIST_OUTDIR}" \
+           --strict_varlist \
+           --pp_comp_glob "*_cmip*" \
+           --overwrite
+
+
+    echo "checking that fremor config's output exists, return if not"
+    ls -l "${FREMOR_CONFIG_OUTYAML}" || return
+fi
+
+
+#### YAML
+#### ----
+# fremor yaml itself doesn't really create output, fremor run does
+# that output directory is passed through the yaml to fremor run
+#FREMOR_YAML_OUTDIR=${OUTPUT_CMORIZED_DATA_DIR}/fremor_yaml_outdir
+if [[ "${CHECK_YAML}" -eq 1 ]]; then
+    echo "not checking fremor yaml"
+else
+    ## lets not do this for this part yet
+    #echo "setting up fremor yaml check, clobbering any prev made output"
+    #rm -rf "${FREMOR_YAML_OUTDIR}" || echo "no yaml output to remove, OK!" && mkdir "${FREMOR_YAML_OUTDIR}"
+
+
+    echo "running fremor yaml"
+    echo "fremor -vv yaml --yamlfile ${FREMOR_CONFIG_OUTYAML} --start ${PP_START} --stop ${PP_STOP} --print_cli_call --run_strict --run_one --dry_run"
+    fremor -vv yaml \
+           --yamlfile "${FREMOR_CONFIG_OUTYAML}" \
+           --start "${PP_START}" \
+           --stop "${PP_STOP}"
+#           --run_strict \
+           --run_one
+#           --dry_run
+
+    echo "checking the output cmorized data directory for successfully created output"
+    tree ${OUTPUT_CMORIZED_DATA_DIR}/*/*/CMIP/
+
+    echo "checking the output cmorized data directory for created output"
+    echo "number of left-behind tmp outputs (without interpolated pressure style coordinate vars is:"
+    ls ${OUTPUT_CMORIZED_DATA_DIR}/*/*/CMOR_tmp/*nc  | grep -v '\.ps\.' | grep -v '\.phalf\.' | grep -v -c '\.pfull\.'
+fi
 #
 #
 #
